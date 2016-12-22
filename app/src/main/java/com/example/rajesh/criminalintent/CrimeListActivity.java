49005b2ed4860ab3090bf3f91f6dcc4ad1d03e29
squashmeister99 <squashmeister99@gmail.com>
2016-12-22 15:17:04 -0700

    added recycler view basic

--- app/src/main/java/com/example/rajesh/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/example/rajesh/criminalintent/CrimeListActivity.java
@@ -2,10 +2,13 @@ package com.example.rajesh.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+/**
+ * Created by Rajesh on 12/22/2016.
+ */
 
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
