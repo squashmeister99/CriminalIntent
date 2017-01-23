f20f499b47456d1116d437f0d191fb551f86bb63
squashmeister99 <squashmeister99@gmail.com>
2017-01-23 10:36:51 -0700

    added shareCompat

--- app/src/main/java/com/example/rajesh/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/rajesh/criminalintent/CrimeFragment.java
@@ -10,6 +10,7 @@ import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -137,8 +138,11 @@ public class CrimeFragment extends Fragment {
         mReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("text/plain");
+
+                Intent i = ShareCompat.IntentBuilder.from(getActivity())
+                .setType("text/plain")
+                        .getIntent();
+
                 i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
                 i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject));
                 i = Intent.createChooser(i, getString(R.string.send_report));
